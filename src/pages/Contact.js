896bceaea799d3de9521cd19e0db19f3d155c2b2
Anna Roos <anna.flytstrom@gmail.com>
2022-01-17 21:33:07 +0100

    starting contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import Button from '@material-ui/core/Button';
@@ -8,6 +8,7 @@ import Typography from '@material-ui/core/Typography';
 
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 
+import airplane from '../assets/send.svg';
 import background from '../assets/background.jpg';
 import phoneIcon from '../assets/phone.svg';
 import emailIcon from '../assets/email.svg';
@@ -20,11 +21,28 @@ const useStyles = makeStyles((theme) => ({
     backgroundRepeat: 'none',
     height: '40rem',
   },
+  messageInput: {
+    border: `2px solid ${theme.palette.common.arcBlue}`,
+    borderRadius: 5,
+    margin: '3rem 0 1rem 0',
+  },
 }));
 
 const Contact = () => {
   const classes = useStyles();
   const theme = useTheme();
+
+  const [enteredName, setEnteredName] = useState('');
+  const [enteredEmail, setEnteredEmail] = useState('');
+  const [enteredPhone, setEnteredPhone] = useState('');
+  const [enteredMessage, setEnteredMessage] = useState('');
+
+  const inputChangeHandler = (event) => {
+    if (event.target.id === 'name') setEnteredName(event.target.value);
+    if (event.target.id === 'email') setEnteredEmail(event.target.value);
+    if (event.target.id === 'phone') setEnteredPhone(event.target.value);
+    if (event.target.id === 'message') setEnteredMessage(event.target.value);
+  };
 
   return (
     <Grid container direction="row">
@@ -72,16 +90,50 @@ const Contact = () => {
             </Typography>
           </Grid>
         </Grid>
-        <Grid comtainer item>
+
+        <Grid container direction="column" item spacing={3}>
           <Grid item>
-            <TextField id="name" label="Name"></TextField>
+            <TextField
+              id="name"
+              label="Name"
+              onChange={(event) => inputChangeHandler(event)}
+              value={enteredName}
+            ></TextField>
           </Grid>
           <Grid item>
-            <TextField id="email" label="Email"></TextField>
+            <TextField
+              id="email"
+              label="Email"
+              onChange={(event) => inputChangeHandler(event)}
+              value={enteredEmail}
+            ></TextField>
           </Grid>
           <Grid item>
-            <TextField id="phone" label="Phone"></TextField>
+            <TextField
+              id="phone"
+              label="Phone"
+              onChange={(event) => inputChangeHandler(event)}
+              value={enteredPhone}
+            ></TextField>
           </Grid>
+        </Grid>
+
+        <Grid item>
+          <TextField
+            className={classes.messageInput}
+            InputProps={{ disableUnderline: true }}
+            id="message"
+            multiline
+            onChange={(event) => inputChangeHandler(event)}
+            placeholder="Write your message"
+            rows={10}
+            value={enteredMessage}
+          ></TextField>
+        </Grid>
+        <Grid item>
+          <Button variant="contained">
+            Send Message <img alt="paper plane" src={airplane} />
+          </Button>
         </Grid>
       </Grid>
 
