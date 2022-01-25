62f8004e61780df6206510b2d0ef12c18f33c610
Anna Roos <anna.flytstrom@gmail.com>
2022-01-25 16:04:04 +0100

    starting info block

--- src/components/buttons/LearnMoreBtn.js
+++ src/components/buttons/LearnMoreBtn.js
@@ -8,12 +8,12 @@ import ButtonArrow from './ButtonArrow';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    borderColor: theme.palette.common.arcBlue,
+    borderColor: (styling) => styling.borderColor,
     borderWidth: '2px',
     borderRadius: '50px',
     height: (styling) => styling.height,
     width: (styling) => styling.width,
-    color: theme.palette.common.arcBlue,
+    color: (styling) => styling.color,
     textTransform: 'none',
     fontSize: (styling) => styling.fontSize,
     fontWeight: 'bold',
@@ -23,18 +23,24 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const LearnMoreBtn = ({ styling = null }) => {
+const LearnMoreBtn = ({ styling = null, arrowStyling = null }) => {
+  const theme = useTheme();
   const customStyles = styling
     ? styling
-    : { fontSize: '0.9rem', height: '3rem', width: '9rem' };
+    : {
+        color: theme.palette.common.arcBlue,
+        borderColor: theme.palette.common.arcBlue,
+        fontSize: '0.9rem',
+        height: '3rem',
+        width: '9rem',
+      };
   const classes = useStyles(customStyles);
-  const theme = useTheme();
 
   return (
     <Button className={classes.root} variant="outlined">
       <span>Learn more</span>
       <ButtonArrow
-        fill={theme.palette.common.arcBlue}
+        fill={arrowStyling ? arrowStyling.color : theme.palette.common.arcBlue}
         height={'1rem'}
         width={'1rem'}
       />
--- src/components/content/InfoBlock.js
+++ src/components/content/InfoBlock.js
@@ -1,7 +1,65 @@
 import React from 'react';
 
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+
+import { makeStyles } from '@material-ui/styles';
+
+import infoBackground from '../../assets/infoBackground.svg';
+import LearnMoreBtn from '../buttons/LearnMoreBtn';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: '100vh',
+    width: '100vw',
+    padding: '5rem',
+    backgroundImage: `url(${infoBackground})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+  },
+}));
+
 const InfoBlock = () => {
-  return <div></div>;
+  const classes = useStyles();
+
+  const learnBtnStyling = {
+    color: 'white',
+    borderColor: 'white',
+    fontSize: '0.7rem',
+    height: '2.5rem',
+    width: '8rem',
+  };
+
+  return (
+    <Grid
+      className={classes.root}
+      container
+      alignItems="center"
+      justifyContent="space-between"
+    >
+      <Grid item>
+        <Grid container direction="column">
+          <Typography>About Us</Typography>
+          <Typography>Let’s get personal.</Typography>
+          <LearnMoreBtn
+            styling={learnBtnStyling}
+            arrowStyling={{ color: 'white' }}
+          />
+        </Grid>
+      </Grid>
+      <Grid item>
+        <Grid container direction="column" alignItems="flex-end">
+          <Typography>Contact Us</Typography>
+          <Typography>Say hello!</Typography>
+          <LearnMoreBtn
+            styling={learnBtnStyling}
+            arrowStyling={{ color: 'white' }}
+          />
+        </Grid>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default InfoBlock;
